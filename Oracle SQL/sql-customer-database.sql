571f144dbeece5335dc86f08e5414b9480accd54
John Ryan <johnjoseph.ryan0@gmail.com>
2021-02-05 21:38:54 +0000

    Update sql-customer-database.sql

--- Oracle SQL/sql-customer-database.sql
+++ Oracle SQL/sql-customer-database.sql
@@ -1,18 +1,18 @@
-###Structured Query Language (SQL) – Commands
+--Structured Query Language (SQL) – Commands
 
-#Create a database
+--Create a database
 CREATE DATABASE Database1;
 
-#View all available databases
+--View all available databases
 SHOW DATABASES;
 
-#Drop/Delete a database
+--Drop/Delete a database
 DROP DATABASE Database1;
 
 
-#Create a Customers Table
+--Create a Customers Table
 
-#USE Database1;
+USE Database1;
 CREATE TABLE CUSTOMERS(
    CustomerID   INT              NOT NULL,
    NAME VARCHAR (20)     NOT NULL,
@@ -22,9 +22,9 @@ CREATE TABLE CUSTOMERS(
    PRIMARY KEY (CustomerID)
 )
 
-#Create the Orders Table and add a relationship
+/*Create the Orders Table and add a relationship*/
 
-#USE Database1;
+USE Database1;
 CREATE TABLE ORDERS(
    OrderID   INT  NOT NULL,
    ITEM VARCHAR (30),
@@ -34,24 +34,24 @@ CREATE TABLE ORDERS(
 )
 
 
+/* Add Relationships
 
+If however, you have the table already created, use the following Alter command
 
-#Add Relationships
-#If however, you have the table already created, use the following Alter command
-
-#To create a FOREIGN KEY constraint on the "P_Id" column when the "Orders" table is already created, use the following SQL: MySQL / SQL Server / Oracle / MS Access:
+To create a FOREIGN KEY constraint on the "P_Id" column when the "Orders" table is already created, use the following SQL: 
+MySQL / SQL Server / Oracle / MS Access: */
 
 ALTER TABLE Orders
 ADD FOREIGN KEY (ItemID)
 REFERENCES Items (ItemID)
 
-#Drop a Table
+/* Drop a Table */
 DROP TABLE CUSTOMERS;
 
-#Alter a table
+/* Alter a table */
 ALTER TABLE CUSTOMERS ADD SEX char(1)
 
-#Insert values into a Table
+/* Insert values into a Table */
 
 USE Database1;
 
@@ -77,25 +77,22 @@ VALUES (101, 'Best of Jay Z', 1, '13.99');
 INSERT INTO ORDERS (OrderID,ITEM, CustomerID,AMOUNT)
 VALUES (102, 'U2 Greatest Hits', 3, '11.99');
 
-
-
 Select values from a table
 USE Database1;
 Select * from CUSTOMERS
 
-#OR
 
 USE Database1;
 Select CustomerID, NAME, SALARY FROM CUSTOMERS
 
 
-#Add a condition
+/* Add a condition */
 
 USE Database1;
 Select CustomerID, NAME, ADDRESS, SALARY FROM CUSTOMERS WHERE SALARY > 1500;
 
 
-Use ‘AND’ 
+--Use ‘AND’ 
 USE Database1;
 SELECT CustomerID, NAME, SALARY 
 FROM CUSTOMERS
@@ -112,21 +109,22 @@ SET ADDRESS = 'Europe'
 WHERE CustomerID = 1;
 
 
-#Delete a record
+/* Delete a record */
 USE Database1;
 DELETE FROM CUSTOMERS
 WHERE CustomerID = 6;
 
-Select records ‘LIKE’
-•	WHERE SALARY LIKE '200%' Finds any values that start with 200
-•	WHERE SALARY LIKE '%200%' Finds any values that have 200 in any position
-•	WHERE SALARY LIKE '_00% 'Finds any values that have 00 in the second and third positions
-•	WHERE SALARY LIKE '2_%_% Finds any values that start with 2 and are at least 3 characters in length
-•	WHERE SALARY LIKE '%2' Finds any values that end with 2
-•	WHERE SALARY LIKE '_2%3' Finds any values that have a 2 in the second position and end with a 3
-•	WHERE SALARY LIKE '2___3' Finds any values in a five-digit number that start with 2 and end with 3
+/*Select records ‘LIKE’*/
+•	WHERE SALARY LIKE '200%' /* Finds any values that start with 200 */
+•	WHERE SALARY LIKE '%200%'/* Finds any values that have 200 in any position */
+•	WHERE SALARY LIKE '_00% '/* Finds any values that have 00 in the second and third positions */
+•	WHERE SALARY LIKE '2_%_%'/* Finds any values that start with 2 and are at least 3 characters in length */
+•	WHERE SALARY LIKE '%2' /*Finds any values that end with 2 */
+•	WHERE SALARY LIKE '_2%3' /* Finds any values that have a 2 in the second position and end with a 3*/
+•	WHERE SALARY LIKE '2___3' /* Finds any values in a five-digit number that start with 2 and end with 3 *//
 
-Select the ‘TOP’ records
+--Select the ‘TOP’ records
+
 USE Database1;
 SELECT * FROM CUSTOMERS
 LIMIT 3;
@@ -137,18 +135,15 @@ SELECT * FROM CUSTOMERS
 ORDER BY NAME DESC;
 
 Group By clause
-If you want to know the total amount of salary for each customer
+--If you want to know the total amount of salary for each customer
 
 USE Database1;
 SELECT NAME, SUM(SALARY) FROM CUSTOMERS
 GROUP BY NAME;
 
-Sorting results
-Sorting first by name and then by salary
+--Sorting results
+--Sorting first by name and then by salary
 
 USE Database1;
 SELECT * FROM CUSTOMERS
 ORDER BY NAME, SALARY
-
-
-
